52a7a73b0740b8a5a984101a2eda24741b34e2ac
Lateralus- <elena.skorchenko.y@gmail.com>
2015-10-25 18:01:49 +0200

    DAL 1

---
+++ EYM/EYM.EntityFramework/Context.cs
@@ -0,0 +1,24 @@
+﻿using EYM.EntityFramework.Interfaces;
+using EYM.Repositories.Interfaces;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EYM.EntityFramework
+{
+    public abstract class Context : DbContext, IContext
+    {
+        protected Context() { }
+
+        protected Context(string connectionString) : base(connectionString) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public new IDbSet<TEntity> Set<TEntity>() where TEntity : class, IEntity
+        {
+            return base.Set<TEntity>();
+        }
+    }
+}
---
+++ EYM/EYM.Repositories.Interfaces/IGenericDataRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace EYM.Repositories.Interfaces
+{
+    public interface IGenericDataRepository<TEntity> : IDisposable
+        where TEntity : class, IEntity
+    {
+        IQueryable<TEntity> GetAll();
+        IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate);
+        TEntity Get(int key);
+        bool Add(TEntity entity);
+        bool Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
