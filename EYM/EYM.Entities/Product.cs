290c20aa8ae9182912331ea10d6b70f02b637d94
chapluck <chapluck@gmail.com>
2015-12-01 19:17:07 +0200

    Revert "Merge pull request #5 from maksii/EYM-7"
    This reverts commit 5d63edb63ae794de61fb0a42e02ef88490049d64, reversing
    changes made to 83c69fbb7ee09d615fe1c9c4ab2e7a33fd17526d.

--- EYM/EYM.Entities/Product.cs
+++ EYM/EYM.Entities/Product.cs
@@ -12,22 +12,12 @@ namespace EYM.Entities
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int Id { get; set; }
 		public double Price { get; set; }
-
-		public int MenuId { get; set; }
+		public DateTime DateToOrder { get; set; }
 
 		public int ProductTemplateId { get; set; }
 
-		public int CategoryId { get; set; }
-		
-		[ForeignKey("MenuId")]
-		public virtual Menu Menu { get; set; }
-
 		[ForeignKey("ProductTemplateId")]
 		public virtual ProductTemplate ProductTemplate { get; set; }
-
-		[ForeignKey("CategoryId")]
-		public virtual Category Category { get; set; }
-
 		public virtual List<OrderLine> OrderLines { get; set; }
 	}
 }
